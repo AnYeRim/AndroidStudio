848440e8b082657996aa8d306582c132b2ac0989
AnYeRim <tmvksals@naver.com>
2023-06-17 22:44:30 +0900

    * use variable & function

--- app/src/main/java/com/study/android/MainActivity.kt
+++ app/src/main/java/com/study/android/MainActivity.kt
@@ -15,17 +15,28 @@ import androidx.compose.ui.unit.dp
 import com.study.android.ui.theme.AndroidStudioTheme
 
 class MainActivity : ComponentActivity() {
+    // 인사말에 사용되는 정보. 변수 선언
+    private val age = 20
+    private val name = "yran"
+    private val year = 1990
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidStudioTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+//                    Greeting(getGreetingText(name, age, year))
+                    Greeting(getGreetingText(age = 29, year = 2034))
                 }
             }
         }
     }
+}
+
+// 인사말 Text 반환하는 함수 선언
+fun getGreetingText(name: String = "gdhong", age: Int, year: Int): String {
+    return "Hi, I'm $name, $age years old. I was born in ${year-age+1}"
 }
 
 /**
@@ -33,8 +44,8 @@ class MainActivity : ComponentActivity() {
  * UI를 생성하는 데 사용된다고 Kotlin 컴파일러에 알림
  **/
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!",
+fun Greeting(greeting: String) {
+    Text(text = greeting,
         color = Color.Red,
         modifier = Modifier.padding(horizontal = 6.dp))
 }
@@ -48,7 +59,7 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     AndroidStudioTheme {
         Surface(color = Color.Yellow) {
-            Greeting("YR AN")
+            Greeting(getGreetingText("YR AN", 27, 2023))
         }
     }
 }
